1c037fc227b467060a9312a77ad28c6bab760710
kevin1010607 <kevin.1010607@gmail.com>
2022-01-04 22:57:05 +0800

    update

--- The night's watch/main.cpp
+++ The night's watch/main.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-int t, n, m, k, res;
+int t, n, m, k;
 int main(void){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -9,21 +9,15 @@ int main(void){
         cin >> n >> m >> k;
         vector<int> v(n);
         for(auto& i : v) cin >> i;
-        if(k >= m-1){
-            int m1 = *max_element(v.begin(), v.begin()+m);
-            int m2 = *max_element(v.rbegin(), v.rbegin()+m);
-            res = max(m1, m2);
-        }
-        else{
-            res = INT_MIN;
-            for(int i = 0; i <= k; i++){
-                int now = INT_MAX;
-                for(int j = 0; j < m-k; j++){
-                    int l = i+j, r = (n-(k-i))-(m-k-j);
-                    now = min(now, max(v[l], v[r]));
-                }
-                res = max(res, now);
+        k = min(k, m-1);
+        int res = 0;
+        for(int i = 0; i <= k; i++){
+            int now = INT_MAX;
+            for(int j = 0; j < m-k; j++){
+                int l = i+j, r = (n-(k-i))-(m-k-j);
+                now = min(now, max(v[l], v[r]));
             }
+            res = max(res, now);
         }
         cout << res << "\n";
     }
--- tmp.cpp
+++ tmp.cpp
@@ -1,32 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef struct{int x,y,u;}S;
-bool connect(S a, S b, int r){
-    return ((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)) <= r*r;
-}
-int dfs(int id, vector<S>& v, int r){
-    int res = 1;
-    v[id].u = 1;
-    for(int i = 0; i < v.size(); i++){
-        if(v[i].u == 1) continue;
-        if(connect(v[id], v[i], r)) res += dfs(i, v, r);
-    }
-    return res;
-}
+int t, n, p, k;
 int main(void){
-    int t, k, n, r;
     cin >> t;
     while(t--){
-        cin >> n >> r >> k;
-        int res1 = 0, res2 = 0;
-        vector<S> v(n);
-        for(auto& i : v) cin >> i.x >> i.y;
-        for(int i = 0; i < n; i++){
-            if(v[i].u == 1) continue;
-            int aa = dfs(i, v, r);
-            aa<k?res1++:res2++;
+        cin >> n >> p >> k;
+        vector<int> v(n), dp(n+1);
+        for(auto& i : v) cin >> i;
+        sort(v.begin(), v.end());
+        for(int i = 1; i <= n; i++){
+            if(i >= k) dp[i] = dp[i-k]+v[i-1];
+            else dp[i] = dp[i-1]+v[i-1];
         }
-        cout << res1 << " " << res2 << "\n";
+        cout << upper_bound(dp.begin(), dp.end(), p)-dp.begin() << "\n";
     }
     return 0;
 }
